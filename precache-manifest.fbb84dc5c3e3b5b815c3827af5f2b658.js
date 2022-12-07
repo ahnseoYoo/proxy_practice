3bfa937226f14e20b40999ac3564519ea98e131d
ahnseoYoo <ahnseo.yoo@gmail.com>
2022-12-07 19:52:01 +0900

    Updates

---
+++ precache-manifest.fbb84dc5c3e3b5b815c3827af5f2b658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5346484ce16f3d4af03e741f0848eea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ab3c3131ec695137fc",
+    "url": "/static/css/main.6e7dcbcc.chunk.css"
+  },
+  {
+    "revision": "6a983529a9d1177ce1fe",
+    "url": "/static/js/2.520c18f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.520c18f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ab3c3131ec695137fc",
+    "url": "/static/js/main.b58279ad.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
